a68f88c976d20cfa7ef1b4dc7ea99cd5e4812e65
Homa Ogundu <homaogundu@gmail.com>
2024-07-03 21:12:51 -0400

    new exercises added

--- src/main/java/com/bptn/course/_week4_exercises/DeleteDuplicates.java
+++ src/main/java/com/bptn/course/_week4_exercises/DeleteDuplicates.java
@@ -1,5 +1,48 @@
 package com.bptn.course._week4_exercises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.Collections;
+
 public class DeleteDuplicates {
 
+	/*
+	 * This method deletes duplicates from an unsorted ArrayList.
+	 * @param nums the sorted ArrayList with duplicates
+	 * @return a sorted ArrayList without duplicates
+	 */
+	public static List<Integer> deleteDuplicates(List<Integer> nums) {
+
+		// Check if the list is null or empty, and return an empty list if so
+		if (nums == null || nums.size() == 0) {
+            return new ArrayList <>();
+		}
+
+        // Implement the bigger details of your algorithm here...
+		 TreeSet<Integer> sortedAndUnique = new TreeSet<>(nums);
+
+		// return sortedAndUnique arraylist
+		 return new ArrayList<>(sortedAndUnique);
+	}
+
+	public static void main(String[] args) {
+		// Initialize a sample sorted ArrayList with duplicate values
+		List<Integer> nums = new ArrayList<>();
+		nums.add(1);
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		nums.add(3);
+		nums.add(3);
+		nums.add(4);
+		nums.add(5);
+		nums.add(5);
+
+		// Call the deleteDuplicates method with the sample list as argument
+		List<Integer> result = deleteDuplicates(nums);
+
+		// Print the result to the console
+		System.out.println(result);
+	}
 }
---
+++ src/main/java/com/bptn/course/_week4_exercises/FibonacciRecursion.java
@@ -0,0 +1,41 @@
+package com.bptn.course._week4_exercises;
+
+public class FibonacciRecursion {
+
+    // Recursive method to calculate the nth Fibonacci number
+    public static int calculateFibonacci(int n) {
+        // Base case 1: Fibonacci(0) is 0
+    	if (n <= 0) {
+    		return 0;
+    	}     
+        // Base case 2: Fibonacci(1) is 1
+    	else if (n == 1) {
+    		return 1;
+    	}
+        // For other values of n, calculate Fibonacci(n) recursively
+    	else {
+    		return calculateFibonacci(n-1) +  calculateFibonacci(n-2);
+    	}
+            // Calculate Fibonacci(n - 1) using recursion
+            
+
+            // Calculate Fibonacci(n - 2) using recursion
+            
+
+            // Calculate Fibonacci(n) by summing the above two values
+             
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("First 10 Fibonacci Numbers:");
+
+        // Loop to calculate and print the first 10 Fibonacci numbers
+        for (int i = 0; i < 10; i++) {
+            int result = calculateFibonacci(i);
+            System.out.println("Fibonacci(" + i + ") = " + result);
+        }
+    }
+}
+
+
